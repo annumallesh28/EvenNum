10e7aa62eeb350fab823fae3eb862717ce12707a
aratimallesh28 <ratimallesh@gmail.com>
2025-05-14 12:06:58 +0530

    this is even number

---
+++ EvenNumbers.java
@@ -0,0 +1,9 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int n = 10;
+        for(int i =0;i<n;i++){
+            System.out.println(2*i);
+        }
+    }
+
+}
